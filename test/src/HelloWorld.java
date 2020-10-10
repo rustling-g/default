afed651bc24058ea1871619765d4a88225768406
rustling-g <ys_guan@zju.edu.cn>
2020-10-10 11:58:15 +0800

    创建分支

--- test/src/HelloWorld.java
+++ test/src/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("更新1");
+        System.out.println("创建分支");
     }
 }
